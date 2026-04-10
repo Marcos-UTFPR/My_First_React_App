b5993f8c008d596901aed0301d38872e65860c7e
Marcos-UTFPR <118696709+Marcos-UTFPR@users.noreply.github.com>
2026-04-10 07:53:50 -0300

    Feat: Atualizado Home/index.tsx para adicionar botões extras.

--- src/app/Home/index.tsx
+++ src/app/Home/index.tsx
@@ -1,27 +1,26 @@
 import { View, Text, Image } from 'react-native' // OBS: Button traz o estilo de botão nativo do Sistema Operacional por default
 
-
 import { Button } from '@/components/Button'
 
-
 import { styles } from './styles'
-
 
 export default function App(){
   return (
     <>
       <View style={styles.container}>
         <Image
-          source={require('@/assets/logo.png')}
+          source={require('@/assets/logo.png')} 
           style={styles.logo}
         />
 
-
-        <Button/>
+        <Button title="Entrar" />
+        <Button title="Cadastrar" />
+        <Button title="Salvar" />
+        <Button title="Voltar" />
+        <Button title="Sair" />
       </View>
     </>
   )
 }
-
 
 // <Text style={styles.text}>Hello, World!!!</Text>
