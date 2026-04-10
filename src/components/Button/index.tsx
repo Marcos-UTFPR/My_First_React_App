aab4816ca4f900c893f5d0a59e48c901d14fb064
Marcos-UTFPR <118696709+Marcos-UTFPR@users.noreply.github.com>
2026-04-10 08:07:48 -0300

    Feat: Atualizado Button/index.tsx para adicionar parâmetro Props do componente.

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,16 +1,19 @@
-import { TouchableOpacity, Text } from 'react-native'
+import { TouchableOpacity, TouchableOpacityProps, Text } from 'react-native'
+// TouchableOpacityProps traz todos os elementos possíveis do botão TouchableOpacity (OBS: Se quiser uma propriedade diferente, tem que criar em baixo)
 
 import { styles } from './styles'
 
+type Props = TouchableOpacityProps & { // Tipagem daquilo que está recebendo
+    title: string  // OBS: title não é propriedade padrão do TouchableOpacity
+}
 
-export function Button(){
+export function Button({ title, ...rest }: Props){  // "...rest" passa todo o que tiver além do title (tudo o que é padrão do botão)
     return(
         <>
-            <TouchableOpacity
-                style={styles.container}
-                activeOpacity={0.7}
-            >
-                <Text style={styles.title}>Entrar</Text>
+            <TouchableOpacity 
+                style={styles.container} {...rest}
+            > 
+                <Text style={styles.title}>{title}</Text>
             </TouchableOpacity>
         </>
     )
