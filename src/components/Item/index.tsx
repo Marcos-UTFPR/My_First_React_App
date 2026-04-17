dc1c40b2c5553d26d5a18373672118225659bc8a
Marcos-UTFPR <118696709+Marcos-UTFPR@users.noreply.github.com>
2026-04-17 09:03:58 -0300

    Feat: Adicionado componente Item.

---
+++ src/components/Item/index.tsx
@@ -0,0 +1,35 @@
+// Bibliotecas externas
+import { TouchableOpacity, View, Text } from 'react-native'
+import { Trash2 } from 'lucide-react-native/icons'
+
+// Bibliotecas internas
+import { styles } from './styles'
+import { StatusIcon } from '../StatusIcon'
+import { FilterStatus } from '../types/FilterStatus'
+
+type ItemData = {
+    status: FilterStatus
+    description: string
+}
+
+type Props = {
+    data: ItemData
+    onStatus: () => void
+    onRemove: () => void
+}
+
+export function Item({ data, onStatus, onRemove }: Props){
+    return (
+        <View style={styles.container}>
+            <TouchableOpacity onPress = {onStatus}>
+                <StatusIcon status={data.status}/>
+            </TouchableOpacity>
+
+            <Text style={styles.description}>{data.description}</Text>
+
+            <TouchableOpacity onPress={onRemove}>
+                <Trash2 size={18} color='#828282'/>
+            </TouchableOpacity>
+        </View>
+    )
+}
